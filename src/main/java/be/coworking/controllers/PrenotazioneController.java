2bc9a01d95d0e44dffcda06d49d74120121c7248
Marianna <121226166+mari-albrici@users.noreply.github.com>
2023-06-06 17:06:05 +0200

    Added exception

--- src/main/java/be/coworking/controllers/PrenotazioneController.java
+++ src/main/java/be/coworking/controllers/PrenotazioneController.java
@@ -2,9 +2,11 @@ package be.coworking.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import be.coworking.exceptions.UnknownLanguage;
 import be.coworking.services.PrenotazioneService;
 
 @RestController
@@ -27,6 +29,11 @@ public class PrenotazioneController {
 	@GetMapping("/en")
 	public String homeEn() {
 		return "Welcome to desk bookings";
+	}
+	
+	@GetMapping("/{lang}")
+	public String handleUnknownLanguage(@PathVariable String lang) {
+		throw new UnknownLanguage(lang);
 	}
 }
 
